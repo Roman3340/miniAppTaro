1a0deceeae323e0344cc07687bb07f1913f2c8a2
Chukaev Roman <roman.chukaev1@gmail.com>
2025-06-30 01:20:55 +0700

    add absolute buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './pages/Header';
@@ -6,10 +7,12 @@ import TarotCardsPage from './pages/TarotCardsPage/TarotCardsPage';
 import DreamPage from './pages/DreamPage/DreamPage';
 import ZodiacPage from './pages/ZodiacPage/ZodiacPage';
 import HelloPage from './pages/HelloPage/HelloPage';
-// import Loader from './components/Loader/Loader';
-
 
 function App() {
+  const [catalogOpen, setCatalogOpen] = React.useState(false);
+
+  const toggleCatalog = () => setCatalogOpen(prev => !prev);
+  const closeCatalog = () => setCatalogOpen(false);
 
   return (
     <Router>
@@ -18,7 +21,6 @@ function App() {
           <Route path="*" element={
             <>
               <Header />
-              {/* <Loader /> */}
               <Routes>
                 <Route path="/" element={<HelloPage />} />
                 <Route path="/natalChart" element={<NatalChartPage />} />
@@ -26,6 +28,35 @@ function App() {
                 <Route path="/dreams" element={<DreamPage />} />
                 <Route path="/zodiac" element={<ZodiacPage />} />
               </Routes>
+
+              {/* Overlay */}
+              {catalogOpen && <div className="overlay" onClick={closeCatalog} />}
+
+              {/* Фиксированная кнопка */}
+              <button
+                className="catalogButton"
+                onClick={toggleCatalog}
+                aria-expanded={catalogOpen}
+                aria-controls="catalogPanel"
+              >
+                Каталог
+              </button>
+
+              <button
+                className="friendButton"
+              >
+                Пригласить друга
+              </button>
+
+              {/* Панель */}
+              <div
+                id="catalogPanel"
+                className={`catalogPanel ${catalogOpen ? 'open' : ''}`}
+                aria-hidden={!catalogOpen}
+              >
+                <button className="catalogItem">Платные расклады и подписка</button>
+                <button className="catalogItem">Как пользоваться приложением</button>
+              </div>
             </>
           } />
         </Routes>
@@ -34,4 +65,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
